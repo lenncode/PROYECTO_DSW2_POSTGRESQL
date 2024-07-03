7ef45fff0ae1b87d22dca3a473b5a76bbc3530e8
lenncode <torresgerttred45@gmail.com>
2024-07-02 19:36:10 -0500

    Avance

--- src/main/java/com/back/rest/entity/Prestamos.java
+++ src/main/java/com/back/rest/entity/Prestamos.java
@@ -3,10 +3,15 @@ package com.back.rest.entity;
 //import javax.persistence.*;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,7 +24,10 @@ public class Prestamos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPrestamo;
-
+    
+    @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
     private Usuarios_Premium idUsuario;
     private Long idLibro;
     private LocalDate fechaPrestamo;
@@ -27,9 +35,9 @@ public class Prestamos {
     private Byte estado;
 
     // Getters y Setters
-    
+
     public Long getIdPrestamo() {
         return idPrestamo;
-}
-    
+    }
+
 }
